71d680c22a2588efb449b1f599911bb9444d63f8
1010524Alex <1010524@student.sae.edu.au>
2023-07-27 16:45:46 +1000

    Added jetpack exhaust animation.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,21 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float MovementSpeed;
     private Rigidbody2D rb;
     private Vector2 MovementDirection;
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
         MovementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
         //check for an input is held
@@ -28,6 +32,32 @@ public class PlayerMovement : MonoBehaviour
         {
             MovementSpeed =500f;
         }
+
+        if (MovementDirection.y > 0)
+        {
+            animator.SetTrigger("Exhaust");
+        }
+
+        if (MovementDirection.x>0)
+        {
+            gameObject.transform.localScale = new Vector3(3, 3, 1);
+            gameObject.transform.localRotation = Quaternion.Euler(0f, 0f, -15f);
+            animator.SetTrigger("Exhaust");
+        }
+        else if (MovementDirection.x<0)
+        {
+            gameObject.transform.localScale = new Vector3(-3, 3, 1);
+            gameObject.transform.localRotation = Quaternion.Euler(0f, 0f, 15f);
+            animator.SetTrigger("Exhaust");
+        }
+        else
+        {
+            gameObject.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+
+        
+
+
         
             
     }
